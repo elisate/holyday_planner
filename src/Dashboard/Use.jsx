53ad0931f56304252e7cde94f19e8a34116819a6
ELISA_PROPERTY <dushimiyimanaelsa@gmail.com>
2023-10-29 21:33:07 +0200

    my change in components

--- src/Dashboard/Use.jsx
+++ src/Dashboard/Use.jsx
@@ -5,7 +5,13 @@ import { useState } from 'react';
 import {FiEdit2} from 'react-icons/fi';
 import {AiFillDelete} from 'react-icons/ai';
 import './use.css';
+import Edituser from './Edituser';
+import Notiflix from "notiflix";
 function Use() {
+    const [isEditModalOpen, setEditModalOpen] = useState(false);
+    const handleEditClick = () => {
+      setEditModalOpen((previsEditModal) => !previsEditModal);
+    };
 const [SiteUser, SetSiteUser] = useState([]);
  const FecthData= async () => {
     try{
@@ -24,8 +30,47 @@ const [SiteUser, SetSiteUser] = useState([]);
         FecthData();
       }, []);
 
+       const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+       const [userToDelete, setUserToDelete] = useState(null);
+       const handleConfirmDelete = async (id) => {
+         try {
+           Notiflix.Confirm.show(
+             "Confirm delete user",
+             "Do you want to delete?",
+             "Yes",
+             "No",
+             async () => {
+               const res = await axios.delete(
+                 `https://holidayplanner.onrender.com/auth/deleteuser/${id}`,
+                 {
+                   headers: {
+                     Authorization: `Bearer ${localStorage.getItem("token")}`,
+                   },
+                 }
+               );
+               window.location.reload();
+             },
+             () => {
+               alert("If you say so...");
+             },
+             {}
+           );
+         } catch (error) {
+           console.log(error);
+         }
+       };
+       const handleDeleteClick = (user) => {
+         setUserToDelete(user);
+         handleConfirmDelete();
+         // setShowDeleteConfirm(true);
+       };
+       const handleCancelDelete = () => {
+         setShowDeleteConfirm(false);
+       };
+
   return (
-    <div className='use1'>
+    <div className="use1">
+      {isEditModalOpen && <Edituser handleEditClick={handleEditClick} />}
       <table>
         <thead>
           <tr className="tab1">
@@ -45,13 +90,24 @@ const [SiteUser, SetSiteUser] = useState([]);
               <td>{item.phoneNumber}</td>
               <td>{item.role}</td>
               <td>
-                <FiEdit2  className='edituser1'/>
+                <FiEdit2 className="edituser1" onClick={handleEditClick} />
               </td>
               <td>
-                <AiFillDelete className='deleteuser1' />
+                <AiFillDelete
+                  className="deleteuser1"
+                  onClick={() => handleConfirmDelete(item._id)}
+                />
               </td>
             </tr>
           ))}
+
+          {showDeleteConfirm && (
+            <div className="popup">
+              <p>Are you sure you want to delete {userToDelete._id}?</p>
+              <button onClick={handleConfirmDelete}>OK</button>
+              <button onClick={handleCancelDelete}>Cancel</button>
+            </div>
+          )}
         </tbody>
       </table>
     </div>
